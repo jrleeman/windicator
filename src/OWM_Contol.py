8451fb53604c6a569dedfed9ea4e7b7d14551ba0
John Leeman <kd5wxb@gmail.com>
2017-05-05 04:14:53 +0000

    Use PyGame for camera capture

--- src/OWM_Contol.py
+++ src/OWM_Contol.py
@@ -1,12 +1,18 @@
 import json
 import os
-from urllib.request import urlopen
-from urllib.parse import quote
+from urllib2 import urlopen
+from urllib import quote
+#from urllib.request import urlopen
+#from urllib.parse import quote
 import time
 import bigeasydriver
 import RPi.GPIO as GPIO
 import numpy as np
-import cv2
+import datetime
+import pygame
+import pygame.camera
+#import cv2
+
 
 def get_weather(query):
     try:
@@ -22,8 +28,8 @@ def get_weather(query):
                        #'pressure':parsed['main']['sea_level'],
                        'windspeed':parsed['wind']['speed'],
                        'winddeg':parsed['wind']['deg']}
-    except:
-        print("ERROR")
+    except Exception as e:
+        print("ERROR: ", e)
         weather = {'temperature':-999,
                    'humidity':-999,
                    #'pressure':parsed['main']['sea_level'],
@@ -45,13 +51,13 @@ def move_camera(current_direction, move):
     """
     if move >0 :
         stepper.enable()
-        stepper.set_direction('ccw')
+        stepper.set_direction('cw')
         actual_move = stepper.move_degrees(move)
         stepper.disable()
 
     elif move < 0:
         stepper.enable()
-        stepper.set_direction('cw')
+        stepper.set_direction('ccw')
         actual_move = stepper.move_degrees(np.abs(move))
         stepper.disable()
 
@@ -62,38 +68,14 @@ def move_camera(current_direction, move):
     return current_direction + move
 
 
-def take_photo():
-    # Camera 0 is the integrated web cam on my netbook
-    camera_port = 0
-
-    #Number of frames to throw away while the camera adjusts to light levels
-    ramp_frames = 30
-
-    # Now we can initialize the camera capture object with the cv2.VideoCapture class.
-    # All it needs is the index to a camera port.
-    camera = cv2.VideoCapture(camera_port)
-
-    # Captures a single image from the camera and returns it in PIL format
-    def get_image():
-     # read is the easiest way to get a full image out of a VideoCapture object.
-     retval, im = camera.read()
-     return im
-
-    # Ramp the camera - these frames will be discarded and are only used to allow v4l2
-    # to adjust light levels, if necessary
-    for i in xrange(ramp_frames):
-     temp = get_image()
-    print("Taking image...")
-    # Take the actual image we want to keep
-    camera_capture = get_image()
-    file = "/pi/home/test_image.png"
-    # A nice feature of the imwrite method is that it will automatically choose the
-    # correct format based on the file extension you provide. Convenient!
-    cv2.imwrite(file, camera_capture)
-
-    # You'll want to release the camera, otherwise you won't be able to create a new
-    # capture object until your script exits
-    del(camera)
+def take_photo(cam):
+    cam.start()
+    time.sleep(1)
+    img = cam.get_image()
+    utc_time = datetime.datetime.utcnow()
+    img_fname = datetime.datetime.strftime(utc_time, 'images/%Y-%m-%dT%H:%M.jpg')
+    pygame.image.save(img, img_fname)
+    cam.stop()
 
 if __name__ == '__main__':
 
@@ -111,7 +93,13 @@ if __name__ == '__main__':
     stepper.set_stepsize('full step')
     stepper.disable()
 
-    # Start with everything pointing at 180
+    # Setup camera
+    pygame.camera.init()
+    print("Cameras: ", pygame.camera.list_cameras())
+    cam = pygame.camera.Camera("/dev/video0",(1024, 576))
+    
+
+    # Start with everything pointing at 180`
     current_direction = 180.
     while True:
         # Get weather
@@ -125,7 +113,7 @@ if __name__ == '__main__':
         move_camera(current_direction, delta_degrees)
 
         # Take photo
-
+        take_photo(cam)
 
         # Wait for next minute
         time.sleep(60)
